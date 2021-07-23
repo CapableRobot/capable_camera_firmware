43a7265c2e5d4ab7da90d64a096172f50257cf4b
Chris Osterwood <osterwood@gmail.com>
2021-07-22 20:53:14 -0400

    Add license headers

--- src/handlers.zig
+++ src/handlers.zig
@@ -1,3 +1,17 @@
+// Copyright 2021 Chris Osterwood for Capable Robot Components, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 const std = @import("std");
 const web = @import("zhp");
 
@@ -13,15 +27,11 @@ pub const MainHandler = struct {
 pub const InfoHandler = struct {
     pub fn get(self: *InfoHandler, request: *web.Request, response: *web.Response) !void {
         try response.headers.append("Content-Type", "application/json");
-        
+
         if (try info.stat()) |stat| {
-            try std.json.stringify(
-                stat, 
-                std.json.StringifyOptions{
-                    .whitespace = .{ .indent = .{ .Space = 2 } },
-                }, 
-                response.stream
-            );
-        }   
+            try std.json.stringify(stat, std.json.StringifyOptions{
+                .whitespace = .{ .indent = .{ .Space = 2 } },
+            }, response.stream);
+        }
     }
 };
