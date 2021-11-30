8ae44adf25e8d8175279b9e39b7298ce2873171d
Chris Osterwood <osterwood@gmail.com>
2021-11-30 14:12:28 -0500

    Include libexif in build

--- build.zig
+++ build.zig
@@ -7,18 +7,46 @@ pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
-
-    const exe = b.addExecutable("capable_camera_firmware", "src/main.zig");
-    exe.linkSystemLibrary("c");
-
-    exe.setTarget(CrossTarget{
+    const target = CrossTarget{
         .cpu_arch = Target.Cpu.Arch.arm,
         .os_tag = Target.Os.Tag.linux,
         .abi = Target.Abi.musleabihf,
+    };
+
+    const exif = b.addStaticLibrary("exif", null);
+    exif.setTarget(target);
+    exif.setBuildMode(mode);
+    exif.linkLibC();
+    exif.addIncludeDir("packages/c");
+    exif.addCSourceFiles(&.{
+        "packages/c/libexif/exif-data.c",
+        "packages/c/libexif/exif-utils.c",
+        "packages/c/libexif/exif-format.c",
+        "packages/c/libexif/exif-content.c",
+        "packages/c/libexif/exif-loader.c",
+        "packages/c/libexif/exif-mnote-data.c",
+        "packages/c/libexif/exif-entry.c",
+        "packages/c/libexif/exif-gps-ifd.c",
+        "packages/c/libexif/exif-tag.c",
+        "packages/c/libexif/exif-ifd.c",
+        "packages/c/libexif/exif-log.c",
+        "packages/c/libexif/exif-mem.c",
+    }, &.{
+        "-Wall",
+        "-W",
+        "-Wstrict-prototypes",
+        "-Wwrite-strings",
+        "-Wno-missing-field-initializers",
     });
 
+    const exe = b.addExecutable("capable_camera_firmware", "src/main.zig");
+    exe.linkSystemLibrary("c");
+    exe.linkLibrary(exif);
+
+    exe.addIncludeDir("packages/c");
     exe.addPackagePath("zhp", "packages/zhp/src/zhp.zig");
 
+    exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
 
