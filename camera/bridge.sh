b521815435b288c8df34a2495c21b3080b2b0891
Chris Niessl <niessl@hellbender.com>
2022-04-01 16:03:05 -0400

    Attempt cmake modifications to support installing configuration files

--- camera/bridge.sh
+++ camera/bridge.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-trap "i2cset -y 1 20 12 0x00 b" EXIT
+trap "i2cset -y 1 0x14 0xC 0x00 b" EXIT
 
 while :  
 do  
 sleep 1
-value=$(i2cget -y 1 20 15 b)
+value=$(i2cget -y 1 0x14 0xF b)
 if [ "$value" == "0xff" ]  
 then
 	echo $value
@@ -14,5 +14,5 @@ then
 fi
 done  
 
-i2cset -y 1 20 12 0xFF b
+i2cset -y 1 0x14 0xC 0xFF b
 setarch linux32 ./build/libcamera-bridge --codec mjpeg --segment 0 -o sck:///tmp/bridge.sock --width 4056 --height 2160 --framerate 10 --tuning-file imx477.json --timeout 0
