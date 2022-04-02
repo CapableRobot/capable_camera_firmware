6c16ad317ea5ad4b94e7ca04f28d6b9a6eb8ff9d
Chris Niessl <niessl@hellbender.com>
2022-04-02 18:21:45 -0400

    Update the bridge.sh script to use config/support files installed in the build directory

--- camera/bridge.sh
+++ camera/bridge.sh
@@ -15,4 +15,4 @@ fi
 done  
 
 i2cset -y 1 0x14 0xC 0xFF b
-setarch linux32 ./build/libcamera-bridge --codec mjpeg --segment 0 -o sck:///tmp/bridge.sock --width 4056 --height 2160 --framerate 10 --tuning-file imx477.json --timeout 0
+setarch linux32 ./libcamera-bridge --codec mjpeg --segment 0 -o sck:///tmp/bridge.sock --width 4056 --height 2160 --framerate 10 --tuning-file imx477.json --timeout 0
