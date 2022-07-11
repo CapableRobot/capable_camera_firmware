2d381334064bff3cb9d86e1d7acf7bcb93ed91cb
Chris Niessl <chris.niessl@mydadel.com>
2022-07-11 10:28:39 -0400

    Update bridge script

--- camera/bridge.sh
+++ camera/bridge.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-setarch linux32 ./libcamera-bridge --codec mjpeg --segment 0 -o /tmp/recording/pic_ --width 4056 --height 2160 --framerate 10 --tuning-file imx477.json --timeout 0
+./libcamera-bridge --codec mjpeg --segment 0 -o /tmp/recording/pic --width 4056 --height 2160 --framerate 10 --tuning-file imx477.json --timeout 0
