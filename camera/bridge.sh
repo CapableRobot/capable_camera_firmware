7351cccc82f65263142312e5e25e5dcac9c9e590
Chris Niessl <chris.niessl@mydadel.com>
2022-07-11 10:39:45 -0400

    Standalone camera bridge app, no longer requires zig endpoint

--- camera/bridge.sh
+++ camera/bridge.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-trap "i2cset -y 1 0x14 0xC 0x00 b" EXIT
-
-while :  
-do  
-sleep 1
-value=$(i2cget -y 1 0x14 0xF b)
-if [ "$value" == "0xff" ]  
-then
-	echo $value
-	echo "GPS Locked"
-	break
-fi
-done  
-
-i2cset -y 1 0x14 0xC 0xFF b
-setarch linux32 ./libcamera-bridge --tuning-file imx477.json --segment 0 --timeout 0 --netconfig
+./libcamera-bridge --codec mjpeg --segment 0 -o /tmp/recording/pic --width 4056 --height 2160 --framerate 10 --tuning-file imx477.json --timeout 0
---
+++ camera/network/file_output.cpp
@@ -0,0 +1,58 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) 2020, Raspberry Pi (Trading) Ltd.
+ *
+ * net_output.cpp - send output over network.
+ */
+#include <iostream>
+#include <iomanip>
+
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <time.h>
+
+#include "file_output.hpp"
+
+FileOutput::FileOutput(VideoOptions const *options) : Output(options)
+{
+  prefix_ = options_->output;
+  //TODO - Assume jpeg formate for now. Otherwise extract 
+  postfix_ = ".jpg";
+}
+
+FileOutput::~FileOutput()
+{
+}
+
+void FileOutput::outputBuffer(void *mem, size_t size, int64_t timestamp_us, uint32_t /*flags*/)
+{
+  static uint32_t picCounter = 0;
+
+  //generate file name
+  //time_t nowTime;
+  //time(&nowTime);
+  //ctime(&nowTime);
+  std::stringstream fileNameGenerator;
+  fileNameGenerator << prefix_ << "_";
+  fileNameGenerator << std::setw(8) << std::setfill('0') << picCounter;
+  fileNameGenerator << postfix_;
+  std::string fullFileName = fileNameGenerator.str();
+  
+  //open file name and assign fd
+  int fd, ret;
+  fd = open(fullFileName.c_str(), O_CREAT|O_WRONLY|O_TRUNC, 0444);
+  
+  if ((ret = write(fd, mem, size)) < 0) {
+    throw std::runtime_error("failed to write data");
+  }
+
+  close(fd);
+  
+  picCounter += 1;
+  if (options_->verbose)
+  {
+    std::cerr << "  wrote " << ret << " bytes\n";
+  }
+}
