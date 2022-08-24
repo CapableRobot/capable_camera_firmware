9f9e06efe56a14e09e8bd7ac37e501d7053b03e6
Chris Niessl <niessl@hellbender.com>
2022-08-23 21:30:43 -0400

    Update configs to support new destination directory

--- camera/bridge.sh
+++ camera/bridge.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-emmcSpace=df | grep /mnt/data | awk '{print $2}'
-minSpace=1000000
-if (( $emmcSpace > $minSpace )) 
+if [ ! -d "/mnt/data/pic" ]
 then
-    echo "Using new emmc based config"
-    if [ ! -d "/mnt/data/pic" ]
-    then
-      mkdir /mnt/data/pic
-    fi
-    cp emmc_config.json config.json
+  mkdir /mnt/data/pic
 fi
 
 ./libcamera-bridge --config config.json --segment 0  --timeout 0 --tuning-file imx477.json --quality 70
